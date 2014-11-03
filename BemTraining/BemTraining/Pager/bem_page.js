2c0ee76f6e9a7881520e8876eb4e79e325be4bbe
ken200 <tisp1982@gmail.com>
2014-11-04 07:05:02 +0900

    ページングコントロールの追加

---
+++ BemTraining/BemTraining/Pager/bem_page.js
@@ -0,0 +1,21 @@
+﻿/// <reference path="../Scripts/typings/jquery/jquery.d.ts" />
+var bempage;
+(function (bempage) {
+    var PageData = (function () {
+        function PageData() {
+        }
+        return PageData;
+    })();
+    bempage.PageData = PageData;
+
+    var Pager = (function () {
+        function Pager() {
+        }
+        Pager.prototype.load = function (data) {
+            this.data = data;
+        };
+        return Pager;
+    })();
+    bempage.Pager = Pager;
+})(bempage || (bempage = {}));
+//# sourceMappingURL=bem_page.js.map
---
+++ BemTraining/BemTraining/Pager/bem_page.ts
@@ -0,0 +1,20 @@
+﻿/// <reference path="../Scripts/typings/jquery/jquery.d.ts" />
+module bempage {
+
+    export class PageData {
+        constructor() { }
+    }
+
+    export class Pager{
+
+        private data: Array<PageData>;
+
+        constructor() {
+        }
+
+        load(data: Array<PageData>) {
+            this.data = data;
+        }
+    }
+
+}
